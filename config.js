7e8941815ca693d3a79e239d9cdf669aab98c9cf
cff-122 <2106685064@qq.com>
2026-06-15 22:32:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GITHUB_TOKEN: "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",
+  GITHUB_TOKEN: "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",
   OWNER: "cff-122",
   REPO: "wx-order"
 };
