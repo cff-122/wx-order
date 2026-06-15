cf01e97eeb9bac689d7be28315bcdafb653686a3
cff-122 <2106685064@qq.com>
2026-06-15 23:34:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GITHUB_TOKEN: "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", // 例如：ghp_AbCdEfGhIjKlMnOpQrStUvWxYz123456
+  GITHUB_TOKEN: "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", // 例如：ghp_AbCdEfGhIjKlMnOpQrStUvWxYz123456
   OWNER: "cff-122",
   REPO: "wx-order"
 };
